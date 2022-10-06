118320e6d020abe79507b473c1f3e8e4b4a6cf57
emran-jatri <emran.jatri@gmail.com>
2022-10-06 16:11:35 +0600

    user main.ts updated

--- blog-user/src/main.ts
+++ blog-user/src/main.ts
@@ -4,8 +4,8 @@ import { Transport } from '@nestjs/microservices/enums';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+	const app = await NestFactory.create(AppModule);
+	app.connectMicroservice({
 		transport: Transport.RMQ,
 		options: {
 			urls: ['amqps://jibzlvbd:QwX5CKtkkKdCtukf72eUxQSUO4vecBzE@dingo.rmq.cloudamqp.com/jibzlvbd'],
@@ -14,11 +14,20 @@ async function bootstrap() {
 				durable: false
 			},
 		},
-	});
+	})
+	
+	// const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+	// 	transport: Transport.RMQ,
+	// 	options: {
+	// 		urls: ['amqps://jibzlvbd:QwX5CKtkkKdCtukf72eUxQSUO4vecBzE@dingo.rmq.cloudamqp.com/jibzlvbd'],
+	// 		queue: 'user_queue',
+	// 		queueOptions: {
+	// 			durable: false
+	// 		},
+	// 	},
+	// });
 
-	// @ts-ignore
-	app.listen(() => {
-		console.log('Microservice is listening');
-	});
+	await app.startAllMicroservices();
+	await app.listen(3000);
 }
 bootstrap();
